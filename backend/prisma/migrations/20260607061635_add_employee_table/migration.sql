281c29dd3dc937f110ca1bd6bad8534a9398f2b3
neetu <nitu@swcinfotech.com>
2026-06-07 11:54:50 +0530

    Added employee table

---
+++ backend/prisma/migrations/20260607061635_add_employee_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
